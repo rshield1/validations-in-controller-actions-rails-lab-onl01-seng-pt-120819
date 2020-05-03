b69f18230f5a2680b55cafd9ebe3ec4b70565196
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-03 13:50:54 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create!(author_params)
-    if !@author.valid?
+    if !@author
       render :new
     else
       redirect_to author_path(@author)
